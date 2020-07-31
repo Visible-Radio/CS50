dc045f41548e722efc156203e735de5523d78893
Visible-Radio <Visible-Radio@users.noreply.github.com>
2020-07-31 12:25:39 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -41,7 +41,7 @@ void print_pairs();
 void print_preferences();
 void mergeSort(pair array[], int left, int right);
 void merge(pair array[], int pleft, int qmiddle, int rright);
-int checkLoser(int loser, int winner, int target);
+int checkLoser(int loser, int winner, int targetW, int targetL);
 
 int main(int argc, string argv[])
 {
@@ -421,7 +421,7 @@ void lock_pairs(void)
 
 
 
-    if (checkLoser(pairs[i].loser, pairs[i].winner, pairs[i].winner) == 0)
+    if (checkLoser(pairs[i].loser, pairs[i].winner, pairs[i].winner, pairs[i].loser) == 0)
     {
         locked[pairs[i].winner][pairs[i].loser] = true;
 
@@ -434,7 +434,7 @@ void lock_pairs(void)
 
 }
 
-int checkLoser(int loser, int winner, int target)
+int checkLoser(int loser, int winner, int targetW, int targetL)
 {
     /*
     checkLoser recieves from lock_pairs() the index of the loser it is looking at
@@ -452,7 +452,7 @@ int checkLoser(int loser, int winner, int target)
   for (int i=0; i < pair_count; i ++)
   {
 
-    if (target == pairs[i].loser /* might need to check if pair is locked here */)
+    if (targetW == pairs[i].winner && targetL == pairs[i].loser /* might need to check if pair is locked here */)
     {
         return 1;
     }
@@ -461,7 +461,7 @@ int checkLoser(int loser, int winner, int target)
 
      if (pairs[i].winner == loser && (locked[pairs[i].winner][pairs[i].loser]==true) )
         {
-            checkLoser(pairs[i].loser, pairs[i].winner, target);
+            checkLoser(pairs[i].loser, pairs[i].winner, targetW, targetL);
         }
 
   }
